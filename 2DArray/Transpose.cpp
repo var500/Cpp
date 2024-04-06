12613c9a7914f1741276767c0c394f1c5af4394e
var500 <varun5chodha@gmail.com>
2024-04-07 03:04:51 +0530

    Fix: square matrix

--- 2DArray/Transpose.cpp
+++ 2DArray/Transpose.cpp
@@ -3,22 +3,29 @@ using namespace std;
 
 int main()
 {
-    int r=3,c=2;
-    int arr[r][c]={{1,2,3},{4,5,6},{7,8,9}};
+    int r = 3, c = 3;
+    int arr[r][c] =
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}};
 
     int arr2[r][c];
-    for(int row=0;row<r;row++){
-        for(int col=0;col<c;col++){
-           
-                arr2[col][row]=arr[row][col];
+    for (int row = 0; row < r; row++)
+    {
+        for (int col = 0; col < c; col++)
+        {
+
+            arr2[col][row] = arr[row][col];
         }
-        
     }
 
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            cout<<arr2[i][j]<<" ";
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cout << arr2[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
