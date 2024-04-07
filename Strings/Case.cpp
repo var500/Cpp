10d8b31cbd26e925d235feab2e94bb8cfd63b93b
var500 <varun5chodha@gmail.com>
2024-04-07 19:24:01 +0530

    String Case

--- Strings/Case.cpp
+++ Strings/Case.cpp
@@ -1,26 +1,46 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+string toUppercase(string s)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+            s[i] = s[i] - 32;
+    }
+
+    return s;
+}
+
+string toLowerCase(string s)
+{
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            s[i] += 32;
+    }
+    return s;
+}
+
+int main()
+{
     string s = "hey There";
-    for(int i=0;i<s.length();i++){
-        if(s[i]>='a' && s[i]<='z')
-            s[i]=s[i]-32;
-    }
-    cout<<s<<endl;
 
-    for(int i=0;i<s.size();i++){
-        if(s[i]>='A' && s[i]<='Z') s[i] +=32;
+    cout << toUppercase(s) << endl;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            s[i] += 32;
     }
 
-    cout<<s<<endl;
+    cout << toLowerCase(s) << endl;
 
     // Using Inbuild Function
     string s1 = "Using The inbuilt Functions";
-    transform(s1.begin(),s1.end(),s1.begin(), ::toupper);
-    cout<<s1<<endl;
-    transform(s1.begin(),s1.end(),s1.begin(), ::tolower);
-    cout<<s1<<endl;
+    // start, end, start function from, to function
+    transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
+    cout << s1 << endl;
+    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+    cout << s1 << endl;
 }
