36f9b2546390cc9cdcc66a02fd4803de315da6dd
var500 <varun5chodha@gmail.com>
2024-04-07 22:17:46 +0530

    Code Formatting

--- BitManupulation/powerof2.cpp
+++ BitManupulation/powerof2.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
-
-int powerof2(int n){
-    return (n&&!(n & n-1));
+/// @brief Code to check if given number is a power of 2
+/// @param n
+/// @return message
+int powerof2(int n)
+{
+    return (n && !(n & n - 1));
 }
-int main(){
-    int num =16;
-    if(powerof2(num)){
-        cout<<"The number is a power of 2";
+int main()
+{
+    int num = 16;
+    if (powerof2(num))
+    {
+        cout << "The number is a power of 2";
     }
 
-    else    cout<<"Not a power of 2";
+    else
+        cout << "Not a power of 2";
 }
