882643de55e617677f2e4c07a212123b9531a360
var500 <varun5chodha@gmail.com>
2024-04-07 19:01:15 +0530

    More String in-built functions

--- Strings/stringFunctions.cpp
+++ Strings/stringFunctions.cpp
@@ -27,6 +27,13 @@ int main()
     s5.clear();
 
     /*
+        Checks if the string is empty of not
+        Returns 1 if true 0 if false
+    */
+
+    cout << s5.empty();
+
+    /*
      compare two strings lexicographically.
      Compares the value of the string object (or a substring) to the sequence of characters specified by its arguments
 
@@ -43,5 +50,44 @@ int main()
     string a2 = "z";
 
     cout << a2.compare(a1);
+
+    /*
+        Erases a part of the string and returns the remaining string
+        Requires starting index and no of char to remove.
+    */
+
+    string a3 = "Thisisalongsentence";
+    a3.erase(3, 3);
+
+    /*
+        Check if the substring exists in the string or not
+        Return starting index of the postion if found else a garbage value
+    */
+
+    cout << a3.find("long");
+
+    // Insert character at a particular index in the string
+    a3.insert(6, "very");
+
+    // Get the no length/ no of characters in the string
+    a3.length();
+    a3.size();
+
+    // Get the substring within the specifies indexes
+    a3.substr(3, 7);
+
+    // Parse String to int
+    string strnum = "786";
+    int num = stoi(strnum); // -> gives 786 as integer
+
+    // Parse int to string
+    int num2 = 786;
+    string strnum2 = to_string(num2);
+    cout << strnum + "2"; // -> "7862"
+
+    // Sort a string. Present in algorithm header file
+    string unsorted = "eadbc";
+    sort(unsorted.begin(), unsorted.end());
+
     return 0;
 }
