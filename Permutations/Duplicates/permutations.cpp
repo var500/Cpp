eebdec7e742fbeb8313a66223428966fa9e6bf80
var500 <varun5chodha@gmail.com>
2024-04-15 23:29:16 +0530

    backtracking and permutation

---
+++ Permutations/Duplicates/permutations.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void helper(vector<int> a, vector<vector<int>> &ans, int idx)
+{
+    if (idx == a.size())
+    {
+        ans.push_back(a);
+        return;
+    }
+
+    for (int i = idx; i < a.size(); i++)
+    {
+        if (i != idx and a[i] == a[idx])
+            continue;
+        swap(a[i], a[idx]);
+        helper(a, ans, idx + 1);
+    }
+}
+
+vector<vector<int>> permute(vector<int> nums)
+{
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    helper(nums, ans, 0);
+    return ans;
+}
+
+int32_t main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &i : a)
+        cin >> i;
+    vector<vector<int>> res = permute(a);
+
+    for (auto v : res)
+    {
+        for (auto i : v)
+        {
+            cout << i << " ";
+        }
+        cout << '\n';
+    }
+}
+
+/*
+    [1,2,2,3]
+    [
+        [1,2,2,3],[1,2,3,2],
+        [1,3,2,2],[2,1,2,3]
+        [2,1,3,2],[2,2,1,3],
+        [2,2,3,1],[3,2,2,1],
+        [3,2,1,2],[3,1,2,2]
+
+    ]
+
+    Time Complexity : O(n!)
+*/
