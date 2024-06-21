9d8c723d2a2ca14efedca30f4280f0bbe85878d8
var500 <varun5chodha@gmail.com>
2024-06-21 22:43:59 +0530

    Update

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 using namespace std;
 
-/*  Insertion sort -
-    Time complexity is O(n^2)  
+/*  Selection sort -
+    Time complexity is O(n^2)
     in both best case and worst case
 */
 
-int main() {
-    int n=6;
-    int arr[n] = {7,45,23,51,19,8};
-    for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[i]){
+int main()
+{
+    int n = 6;
+    int arr[n] = {7, 45, 23, 51, 19, 8};
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
                 int temp = arr[j];
-                arr[j]=arr[i];
-                arr[i]=temp;
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
         }
     }
 
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
     }
 }
