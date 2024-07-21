3a31c5fc9c79c38c1b91ec8640dde34cf06f9e73
var500 <varun5chodha@gmail.com>
2024-07-21 22:31:47 +0530

    improved code

--- ArrayProblems/SmallestMissing.cpp
+++ ArrayProblems/SmallestMissing.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n=8;
-    int arr[n]={0,1,8,7,2,3,5,6};
-    
+int main()
+{
+    int n = 8;
+    int arr[n] = {0, 1, 8, 7, 2, 3, 5, 6};
+
     const int N = 1e6 + 2;
-    bool check[N];
+    bool check[N] = {false};
 
+    // Initially set all array items to false
 
-    // Initially set all array items to false 
-    for(int i=0;i<N;i++)
+    // Set non negative occourance as true
+    for (int i = 0; i < n; i++)
     {
-        check[i]=false;
-    }
-
-    //Set non negative occourance as true
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i]>=0) {
+        if (arr[i] >= 0)
+        {
             check[arr[i]] = 1;
-        } 
-            
+        }
     }
     int ans = -1;
 
-    for(int i=1;i<N;i++){
-        if(check[i]==false){
+    for (int i = 1; i < N; i++)
+    {
+        if (check[i] == false)
+        {
             ans = i;
             break;
         }
     }
 
-    cout<<"ANS: "<<ans;
+    cout << "ANS: " << ans;
     return 0;
 }
